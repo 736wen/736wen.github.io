3110a5953c2d87f1c413d6d8e01586ec7f8cb4e4
736wen <736wen@users.noreply.github.com>
2019-10-18 16:14:32 +0800

    1
    1

--- js/i18n.js
+++
@@ -1,7 +0,0 @@
-window.__I18N__ = (() => {
-    const I18N = {};
-    const lang = navigator.language.toLocaleLowerCase();
-    <!-- lang -->
-    let source = I18N[lang] ? I18N[lang] : {};
-    return key => source[key]||key;
-})();
